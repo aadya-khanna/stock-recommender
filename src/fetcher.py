d194e2e6d9e9bff642c7e980c4f116c8b2df358b
Aadya <102467098+aadya-khanna@users.noreply.github.com>
2025-10-07 15:44:59 -0400

    displaying fetched data for tickers

---
+++ src/fetcher.py
@@ -0,0 +1,28 @@
+from concurrent.futures import ThreadPoolExecutor
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+print("Fetcher module loaded.")
+
+tickers_list = ["AAPL", "MSFT", "TSLA"]
+
+def get_stats():
+    hist_dict = {} 
+    for t in tickers_list:
+        stock = yf.Ticker(t)
+        stock_history = stock.history(period="1mo", interval="1d")
+        hist_dict[t] = stock_history
+    return hist_dict
+
+data = get_stats()
+
+print(type(data))
+
+# metrics to track - avg return, volatility, momentum, P/E, dividend yielf 
+
+# metrics_dict = {}
+
+
+
+
